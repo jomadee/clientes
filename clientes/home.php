3ce16d8c6fea13000bde8c45d157305829ecc876
jeison Frasson <jomadee@lliure.com.br>
2014-08-08 16:51:28 -0300

    4.1

--- clientes/home.php
+++ clientes/home.php
@@ -3,7 +3,7 @@
 *
 * Aplicativo de Clientes | lliure 6.x
 *
-* @versão 4.0
+* @Versão 4.1
 * @Desenvolvedor Jeison Frasson <jomadee@lliure.com.br>
 * @entre em contato com o desenvolvedor <jomadee@lliure.com.br> http://www.lliure.com.br/
 * @licença http://opensource.org/licenses/gpl-license.php GNU Public License
--- clientes/onserver.php
+++ clientes/onserver.php
@@ -3,17 +3,16 @@
 *
 * Aplicativo de Clientes | lliure 6.x
 *
-* @versão 4.0
+* @Versão 4.1
 * @Desenvolvedor Jeison Frasson <jomadee@lliure.com.br>
 * @entre em contato com o desenvolvedor <jomadee@lliure.com.br> http://www.lliure.com.br/
 * @licença http://opensource.org/licenses/gpl-license.php GNU Public License
 *
 */
 
-switch(isset($_GET['ac']) ? : 'erro'){
+switch(isset($_GET['ac']) ? $_GET['ac'] : 'erro'){
 case 'new':
-	echo jf_insert(PREFIXO.'clientes', array('nome' => 'Novo cliente'));
-	
+	echo jf_insert(PREFIXO.'clientes', array('nome' => 'Novo cliente'));	
 	break;
 	
 case 'erro':		
